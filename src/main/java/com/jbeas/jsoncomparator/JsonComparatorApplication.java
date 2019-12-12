86b42a7b23cb4a9bd67401412d7c2beff91dd6d5
jose beas <j.antonio.beas@gmail.com>
2019-12-12 13:09:08 -0600

    Spring configuration and RestHealthcheckController added

--- src/main/java/com/jbeas/jsoncomparator/JsonComparatorApplication.java
+++ src/main/java/com/jbeas/jsoncomparator/JsonComparatorApplication.java
@@ -2,7 +2,11 @@ package com.jbeas.jsoncomparator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan(basePackages = "com.jbeas.jsoncomparator")
 @SpringBootApplication
 public class JsonComparatorApplication {
 
