ec7496839cbdb9e57f461daca986bbfd4d7805f0
jose beas <j.antonio.beas@gmail.com>
2019-12-12 12:16:33 -0600

    initial project creation (springboot and maven support added)

---
+++ src/main/java/com/josebeas/jsoncomparator/JsonComparatorApplication.java
@@ -0,0 +1,13 @@
+package com.josebeas.jsoncomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonComparatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josebeas/jsoncomparator/JsonComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josebeas.jsoncomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
