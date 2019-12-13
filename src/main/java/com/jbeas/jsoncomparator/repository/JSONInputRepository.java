903701901ee8519f9a0e0109b61116fc06bdb827
jose beas <j.antonio.beas@gmail.com>
2019-12-13 13:07:54 -0600

    Basic repositiries added

---
+++ src/main/java/com/jbeas/jsoncomparator/repository/JSONInputRepository.java
@@ -0,0 +1,43 @@
+package com.jbeas.jsoncomparator.repository;
+
+import com.jbeas.jsoncomparator.dto.InputType;
+import com.jbeas.jsoncomparator.dto.JSONInput;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component //TODO: Move this into a @Repository
+public class JSONInputRepository {
+
+    //TODO: move this into a hibernet handled EM and a MySQL DB
+    HashMap<Integer, JSONInput> leftEntityManager = new HashMap<>();
+    HashMap<Integer, JSONInput> rightEntityManager = new HashMap<>();
+
+    //TODO: add custom exceptions when entity not found
+    public JSONInput getById(Integer id, InputType type){
+        //TODO: create a empty constructor defaulting values
+        JSONInput result = new JSONInput(-1, null, null);
+        switch(type){
+            case LEFT:
+                result = leftEntityManager.get(id);
+                break;
+            case RIGHT:
+                result = rightEntityManager.get(id);
+                break;
+        }
+        return result;
+    }
+
+    //TODO: throw custom exceptions when entity not valid
+    public void add(JSONInput entity){
+        if(entity != null)
+            switch(entity.getType()){
+                case LEFT:
+                    leftEntityManager.put(entity.getId(), entity);
+                    break;
+                case RIGHT:
+                    rightEntityManager.put(entity.getId(), entity);
+                    break;
+            }
+    }
+}
